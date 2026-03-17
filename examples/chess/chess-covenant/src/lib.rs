0729159c12591f9d76f480ae24d75613e61e3578
Michael Sutton <msutton@cs.huji.ac.il>
2026-03-17 01:14:07 +0000

    add muxed chess baseline with pawn and knight routes

--- examples/chess/chess-covenant/src/lib.rs
+++ examples/chess/chess-covenant/src/lib.rs
@@ -3,3 +3,15 @@ pub mod model;
 pub fn example_contract_path() -> &'static str {
     concat!(env!("CARGO_MANIFEST_DIR"), "/../sil/chess_game.sil")
 }
+
+pub fn mux_contract_path() -> &'static str {
+    concat!(env!("CARGO_MANIFEST_DIR"), "/../sil/chess_mux.sil")
+}
+
+pub fn pawn_contract_path() -> &'static str {
+    concat!(env!("CARGO_MANIFEST_DIR"), "/../sil/chess_pawn.sil")
+}
+
+pub fn knight_contract_path() -> &'static str {
+    concat!(env!("CARGO_MANIFEST_DIR"), "/../sil/chess_knight.sil")
+}
