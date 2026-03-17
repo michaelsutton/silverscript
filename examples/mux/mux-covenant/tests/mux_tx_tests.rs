c0a0b697378a17dd7af6c54c779f466a9fdc6093
Michael Sutton <msutton@cs.huji.ac.il>
2026-03-17 00:59:23 +0000

    manually lower mux example through singleton policy wrappers

--- examples/mux/mux-covenant/tests/mux_tx_tests.rs
+++ examples/mux/mux-covenant/tests/mux_tx_tests.rs
@@ -1,15 +1,20 @@
 use std::fs;
 
 use blake2b_simd::Params as Blake2bParams;
+use kaspa_consensus_core::hashing::sighash::SigHashReusedValuesUnsync;
 use kaspa_consensus_core::tx::{
-    Transaction, TransactionId, TransactionInput, TransactionOutpoint, TransactionOutput, UtxoEntry, VerifiableTransaction,
+    CovenantBinding, GenesisCovenantGroup, PopulatedTransaction, Transaction, TransactionId, TransactionInput, TransactionOutpoint,
+    TransactionOutput, UtxoEntry, VerifiableTransaction,
 };
-use kaspa_txscript::{pay_to_script_hash_script, pay_to_script_hash_signature_script};
+use kaspa_consensus_core::Hash;
+use kaspa_txscript::caches::Cache;
+use kaspa_txscript::covenants::CovenantsContext;
+use kaspa_txscript::{pay_to_script_hash_script, pay_to_script_hash_signature_script, EngineCtx, EngineFlags, TxScriptEngine};
+use kaspa_txscript_errors::TxScriptError;
 use silverscript_lang::ast::Expr;
 use silverscript_lang::compiler::{compile_contract, CompileOptions, CompiledContract};
 
 use mux_covenant::{mux_contract_path, worker_a_contract_path, worker_b_contract_path};
-
 fn load_contract_source(path: &'static str) -> String {
     fs::read_to_string(path).unwrap_or_else(|err| panic!("failed to read {path}: {err}"))
 }
@@ -28,38 +33,71 @@ fn test_input(index: u32, signature_script: Vec<u8>) -> TransactionInput {
         previous_outpoint: TransactionOutpoint { transaction_id: TransactionId::from_bytes([index as u8 + 1; 32]), index },
         signature_script,
         sequence: 0,
-        sig_op_count: 0,
+        sig_op_count: 1,
     }
 }
 
-fn execute_input(tx: Transaction, entries: Vec<UtxoEntry>, input_idx: usize) -> Result<(), kaspa_txscript_errors::TxScriptError> {
-    let reused_values = kaspa_consensus_core::hashing::sighash::SigHashReusedValuesUnsync::new();
-    let sig_cache = kaspa_txscript::caches::Cache::new(10_000);
+fn covenant_output(compiled: &CompiledContract<'_>, authorizing_input: u16, covenant_id: Hash) -> TransactionOutput {
+    TransactionOutput {
+        value: 1_000,
+        script_public_key: pay_to_script_hash_script(&compiled.script),
+        covenant: Some(CovenantBinding { authorizing_input, covenant_id }),
+    }
+}
+
+fn covenant_utxo(compiled: &CompiledContract<'_>, covenant_id: Hash) -> UtxoEntry {
+    UtxoEntry::new(1_500, pay_to_script_hash_script(&compiled.script), 0, false, Some(covenant_id))
+}
+
+fn populate_single_output_genesis_covenant(compiled: &CompiledContract<'_>) -> Hash {
+    let input = TransactionInput {
+        previous_outpoint: TransactionOutpoint { transaction_id: TransactionId::from_bytes([0x42u8; 32]), index: 0 },
+        signature_script: vec![],
+        sequence: 0,
+        sig_op_count: 0,
+    };
+    let output = TransactionOutput { value: 1_000, script_public_key: pay_to_script_hash_script(&compiled.script), covenant: None };
+    let mut tx = Transaction::new(1, vec![input], vec![output], 0, Default::default(), 0, vec![]);
+    tx.populate_genesis_covenants(&[GenesisCovenantGroup::new(0, vec![0])]).expect("populate genesis covenant");
+    let genesis_utxo = UtxoEntry::new(1_500, Default::default(), 0, false, None);
+    let populated = PopulatedTransaction::new(&tx, vec![genesis_utxo]);
+    CovenantsContext::from_tx(&populated).expect("validate genesis covenant bindings");
+    tx.outputs[0].covenant.expect("genesis output covenant").covenant_id
+}
+
+fn execute_input_with_covenants(tx: Transaction, entries: Vec<UtxoEntry>, input_idx: usize) -> Result<(), TxScriptError> {
+    let reused_values = SigHashReusedValuesUnsync::new();
+    let sig_cache = Cache::new(10_000);
     let input = tx.inputs[input_idx].clone();
-    let populated = kaspa_consensus_core::tx::PopulatedTransaction::new(&tx, entries);
+    let populated = PopulatedTransaction::new(&tx, entries);
+    let cov_ctx = CovenantsContext::from_tx(&populated).map_err(TxScriptError::from)?;
     let utxo = populated.utxo(input_idx).expect("selected input utxo");
 
-    let mut vm = kaspa_txscript::TxScriptEngine::from_transaction_input(
+    let mut vm = TxScriptEngine::from_transaction_input(
         &populated,
         &input,
         input_idx,
         utxo,
-        kaspa_txscript::EngineCtx::new(&sig_cache).with_reused(&reused_values),
-        kaspa_txscript::EngineFlags { covenants_enabled: true },
+        EngineCtx::new(&sig_cache).with_reused(&reused_values).with_covenants_ctx(&cov_ctx),
+        EngineFlags { covenants_enabled: true },
     );
     vm.execute()
 }
 
-fn run_p2sh_transition(input_contract: &CompiledContract, function: &str, args: Vec<Expr<'_>>, output_contract: &CompiledContract) {
+fn run_p2sh_transition(
+    input_contract: &CompiledContract,
+    function: &str,
+    args: Vec<Expr<'_>>,
+    output_contract: &CompiledContract,
+    covenant_id: Hash,
+) {
     let sigscript = input_contract.build_sig_script(function, args).expect("sigscript builds");
     let sigscript = pay_to_script_hash_signature_script(input_contract.script.clone(), sigscript).unwrap();
     let input = test_input(0, sigscript);
-    let input_spk = pay_to_script_hash_script(&input_contract.script);
-    let output_spk = pay_to_script_hash_script(&output_contract.script);
-    let output = TransactionOutput { value: 1000, script_public_key: output_spk, covenant: None };
+    let output = covenant_output(output_contract, 0, covenant_id);
     let tx = Transaction::new(1, vec![input], vec![output.clone()], 0, Default::default(), 0, vec![]);
-    let utxo_entry = UtxoEntry::new(output.value, input_spk, 0, tx.is_coinbase(), None);
-    let result = execute_input(tx, vec![utxo_entry], 0);
+    let utxo_entry = covenant_utxo(input_contract, covenant_id);
+    let result = execute_input_with_covenants(tx, vec![utxo_entry], 0);
     assert!(result.is_ok(), "{function} runtime failed: {}", result.unwrap_err());
 }
 
@@ -76,8 +114,6 @@ struct MuxFixture {
     b_prefix: Vec<u8>,
     b_suffix: Vec<u8>,
     b_hash: Vec<u8>,
-    next_a_hash: Vec<u8>,
-    next_b_hash: Vec<u8>,
 }
 
 fn build_mux_fixture() -> MuxFixture {
@@ -91,8 +127,6 @@ fn build_mux_fixture() -> MuxFixture {
         template_parts_and_hash(&a_source, &[vec![0x41u8; 32].into(), vec![0x51u8; 32].into(), vec![0x61u8; 32].into(), 5.into()]);
     let (b_prefix, b_suffix, b_hash) =
         template_parts_and_hash(&b_source, &[vec![0x71u8; 32].into(), vec![0x81u8; 32].into(), vec![0x91u8; 32].into(), 5.into()]);
-    let next_a_hash = vec![0xa1u8; 32];
-    let next_b_hash = vec![0xb2u8; 32];
 
     MuxFixture {
         mux_source,
@@ -107,8 +141,6 @@ fn build_mux_fixture() -> MuxFixture {
         b_prefix,
         b_suffix,
         b_hash,
-        next_a_hash,
-        next_b_hash,
     }
 }
 
@@ -121,33 +153,36 @@ fn mux_routes_and_workers_return_to_mux() {
     let b_fee = 1;
 
     let mux = compile_contract(&fix.mux_source, &state, CompileOptions::default()).expect("compile mux succeeds");
+    let covenant_id = populate_single_output_genesis_covenant(&mux);
     let a = compile_contract(&fix.a_source, &state, CompileOptions::default()).expect("compile A succeeds");
     let b = compile_contract(&fix.b_source, &state, CompileOptions::default()).expect("compile B succeeds");
     let mux_after_a = compile_contract(
         &fix.mux_source,
-        &[fix.mux_hash.clone().into(), fix.next_a_hash.clone().into(), fix.b_hash.clone().into(), (5 + a_reward).into()],
+        &[fix.mux_hash.clone().into(), fix.a_hash.clone().into(), fix.b_hash.clone().into(), (5 + a_reward).into()],
         CompileOptions::default(),
     )
     .expect("compile mux after A succeeds");
     let mux_after_b = compile_contract(
         &fix.mux_source,
-        &[fix.mux_hash.clone().into(), fix.a_hash.clone().into(), fix.next_b_hash.clone().into(), (5 + b_gain - b_fee).into()],
+        &[fix.mux_hash.clone().into(), fix.a_hash.clone().into(), fix.b_hash.clone().into(), (5 + b_gain - b_fee).into()],
         CompileOptions::default(),
     )
     .expect("compile mux after B succeeds");
 
-    run_p2sh_transition(&mux, "route", vec![0.into(), fix.a_prefix.clone().into(), fix.a_suffix.clone().into()], &a);
-    run_p2sh_transition(&mux, "route", vec![1.into(), fix.b_prefix.clone().into(), fix.b_suffix.clone().into()], &b);
+    run_p2sh_transition(&mux, "route", vec![0.into(), fix.a_prefix.clone().into(), fix.a_suffix.clone().into()], &a, covenant_id);
+    run_p2sh_transition(&mux, "route", vec![1.into(), fix.b_prefix.clone().into(), fix.b_suffix.clone().into()], &b, covenant_id);
     run_p2sh_transition(
         &a,
         "apply",
-        vec![a_reward.into(), fix.next_a_hash.into(), fix.mux_prefix.clone().into(), fix.mux_suffix.clone().into()],
+        vec![a_reward.into(), fix.mux_prefix.clone().into(), fix.mux_suffix.clone().into()],
         &mux_after_a,
+        covenant_id,
     );
     run_p2sh_transition(
         &b,
         "apply",
-        vec![b_gain.into(), b_fee.into(), fix.next_b_hash.into(), fix.mux_prefix.into(), fix.mux_suffix.into()],
+        vec![b_gain.into(), b_fee.into(), fix.mux_prefix.into(), fix.mux_suffix.into()],
         &mux_after_b,
+        covenant_id,
     );
 }
